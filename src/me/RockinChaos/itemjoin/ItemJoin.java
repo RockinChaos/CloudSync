9304765dfb949a4a6d6d27ec7524ccd78935470a
RockinChaos <JacobGaming@CraftationGaming.com>
2021-03-27 21:02:59 -0700

    Cleanup

--- src/me/RockinChaos/itemjoin/ItemJoin.java
+++ src/me/RockinChaos/itemjoin/ItemJoin.java
@@ -1,6 +1,24 @@
+/*
+ * ItemJoin-Bungee
+ * Copyright (C) CraftationGaming <https://www.craftationgaming.com/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package me.RockinChaos.itemjoin;
 
-import me.RockinChaos.itemjoin.listeners.PluginMessage;
+import me.RockinChaos.itemjoin.listeners.Messages;
+import me.RockinChaos.itemjoin.utils.api.MetricsAPI;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class ItemJoin extends Plugin {
@@ -33,7 +51,7 @@ public class ItemJoin extends Plugin {
     */
     @Override
     public void onDisable() {
-    	this.getProxy().unregisterChannel(PLUGIN_CHANNEL);
+    	this.getProxy().unregisterChannel(this.PLUGIN_CHANNEL);
     }
     
    /**
--- src/me/RockinChaos/itemjoin/listeners/Messages.java
+++ src/me/RockinChaos/itemjoin/listeners/Messages.java
@@ -1,3 +1,20 @@
+/*
+ * ItemJoin-Bungee
+ * Copyright (C) CraftationGaming <https://www.craftationgaming.com/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package me.RockinChaos.itemjoin.listeners;
 
 import java.io.ByteArrayInputStream;
@@ -14,7 +31,7 @@ import net.md_5.bungee.api.event.PluginMessageEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
-public class PluginMessage implements Listener {
+public class Messages implements Listener {
 
    /**
     * Called a message is sent to the specified CHANNEL.
