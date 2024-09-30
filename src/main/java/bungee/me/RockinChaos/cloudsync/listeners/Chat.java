7a0a7d959b40de2de7208509ea4f720e20eb528b
RockinChaos <JacobGaming@CraftationGaming.com>
2024-09-29 17:59:20 -0700

    chore: changed pathing

---
+++ src/main/java/bungee/me/RockinChaos/cloudsync/listeners/Chat.java
@@ -0,0 +1,14 @@
+package bungee.me.RockinChaos.cloudsync.listeners;
+
+import net.md_5.bungee.api.event.ChatEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
+import net.md_5.bungee.event.EventPriority;
+
+public class Chat implements Listener {
+
+  @EventHandler(priority = EventPriority.HIGHEST)
+  public void onPlayerChat(ChatEvent event) {
+	  event.setCancelled(false);
+  }
+}
