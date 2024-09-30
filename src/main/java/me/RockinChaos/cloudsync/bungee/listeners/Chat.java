f13c27229e7a2571886a86136f84fdd07726f97d
SpigotRCE <spigotrce@gmail.com>
2024-09-30 11:57:13 +0530

    changed package name

--- src/main/java/me/RockinChaos/cloudsync/bungee/listeners/Chat.java
+++ src/main/java/me/RockinChaos/cloudsync/bungee/listeners/Chat.java
@@ -1,4 +1,4 @@
-package bungee.me.RockinChaos.cloudsync.listeners;
+package src.main.java.me.RockinChaos.cloudsync.bungee.listeners;
 
 import net.md_5.bungee.api.event.ChatEvent;
 import net.md_5.bungee.api.plugin.Listener;
--- src/main/java/me/RockinChaos/cloudsync/bungee/utils/ServerUtils.java
+++ src/main/java/me/RockinChaos/cloudsync/bungee/utils/ServerUtils.java
@@ -15,9 +15,9 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package bungee.me.RockinChaos.cloudsync.utils;
+package src.main.java.me.RockinChaos.cloudsync.bungee.utils;
 
-import bungee.me.RockinChaos.cloudsync.utils.api.SnapAPI;
+import src.main.java.me.RockinChaos.cloudsync.bungee.utils.api.SnapAPI;
 
 public class ServerUtils {
 	
